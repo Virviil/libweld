13dae8e2a0a3f1612af15e1d4b5839d669caa41c
Dmitry Rubinstein <rubinsteindb@gmail.com>
2020-04-29 19:01:45 +0300

    Adding basic specs

--- lib/weld/native.ex
+++ lib/weld/native.ex
@@ -1,12 +1,27 @@
 defmodule Weld.Native do
   use Rustler, otp_app: :libweld, crate: :weld_native
 
-  def weld_module_compile(code, conf), do: error
-  def weld_module_run(module, conf, input_value), do: error
+  @type weld_config :: reference()
+  @type weld_module :: reference()
+  @type weld_value :: reference()
 
-  def weld_conf_new(), do: error
-  def weld_conf_get(conf, key), do: error
-  def weld_conf_set(conf, key, value), do: error
+  @spec weld_module_compile(code :: String.t(), config :: weld_config()) ::
+          {:ok, module :: weld_module()} | {:error, reason :: String.t()}
+  def weld_module_compile(_code, _config), do: error()
+
+  @spec weld_module_run(
+          module :: weld_module(),
+          config :: weld_config(),
+          input_value :: weld_value()
+        ) :: {:ok, result :: weld_value()} | {:error, reason :: String.t()}
+  def weld_module_run(_module, _config, _input_value), do: error()
+
+  @spec weld_conf_new :: weld_config()
+  def weld_conf_new(), do: error()
+  @spec weld_conf_get(config :: weld_config(), key :: String.t()) :: nil | String.t()
+  def weld_conf_get(_config, _key), do: error()
+  @spec weld_conf_set(config :: weld_config(), key :: String.t(), value :: String.t()) :: :ok
+  def weld_conf_set(_config, _key, _value), do: error()
 
   defp error, do: :erlang.nif_error(:nif_not_loaded)
 end
